b92e0ec238a87ddb869b6325a3f0f8130255e99f
sachindolase <sachindolase@gmail.com>
2023-07-03 13:11:49 +0530

    tailwind css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const burgerElement = document.getElementById('burger');
+const menuElement = document.getElementById('menu');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+burgerElement.addEventListener('click', () => {
+    if (menuElement.classList.contains('hidden')) {
+      menuElement.classList.remove('hidden');
+    } else {
+      menuElement.classList.add('hidden');
+    }
+});
